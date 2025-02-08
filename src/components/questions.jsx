34b550195482b431a08a7f1ee5fe9ac822d218ae
Akshat Sharma <86612849+Akshat1804@users.noreply.github.com>
2025-02-09 00:39:08 +0530

    Add files via upload

---
+++ src/components/questions.jsx
@@ -0,0 +1,29 @@
+const questions = [
+    {
+      question: "What is the main contributor to global carbon emissions?",
+      options: [
+        "Transportation",
+        "Agriculture",
+        "Energy production",
+        "Deforestation",
+      ],
+      answer: "Energy production",
+    },
+    {
+      question: "Which gas is the primary component of greenhouse gases?",
+      options: ["Methane", "Oxygen", "Carbon Dioxide", "Nitrogen"],
+      answer: "Carbon Dioxide",
+    },
+    {
+      question: "What is a carbon footprint?",
+      options: [
+        "The total amount of CO2 emissions from human activities",
+        "The number of trees cut down in a year",
+        "The amount of waste generated per person",
+        "A person's weight in carbon molecules",
+      ],
+      answer: "The total amount of CO2 emissions from human activities",
+    },
+  ];
+  
+  export default questions;
