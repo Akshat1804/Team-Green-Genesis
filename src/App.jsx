b6c1de3ba45274dfaf2dcbed66eba289e41b441e
Akshat Sharma <86612849+Akshat1804@users.noreply.github.com>
2025-02-08 22:34:22 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'    
 import NavBar from './components/NavBar'
-import LeftBar from './components/LeftBar'
-import Home from './Pages/Home'
 import Dashboard from './Pages/Dashboard'
+import AuthPage from './Auth/Authpage'
 
 const App = () => {
   return (
     <div className='h-screen'>
         <NavBar/>
-        {/* <LeftBar/> */}
         <Routes>
-            <Route path='/' element= {<Home/>} /> 
+            <Route path='/'element={<AuthPage/>} />
             <Route path='/dashboard' element= {<Dashboard/>} /> 
 
         </Routes>
