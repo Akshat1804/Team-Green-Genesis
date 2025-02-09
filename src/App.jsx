904bb306d907956f2d9653647a89a5861a77eb92
Akshat Sharma <86612849+Akshat1804@users.noreply.github.com>
2025-02-09 13:33:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'    
-import NavBar from './components/NavBar'
-import Dashboard from './Pages/Dashboard'
-import AuthPage from './Auth/Authpage'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+// import NavBar from "./components/NavBar";
+import Dashboard from "./Pages/Dashboard";
+import AuthPage from "./Auth/Authpage";
+import Quiz from "./Pages/Quiz";
+import Game from "./Pages/game";
+import ProjectCollab from "./Pages/ProjectCollab";
+import Map from "./Pages/Map";
+import Calculator from "./Pages/Calculator";
+import { StreakPointsProvider } from "./Context/StreakPointsContext";
+import EcoScore from "./Pages/EcoScore";
+import Tracker from "./Pages/Tracker";
+import AutomatedReminders from "./Pages/AutomatedReminders";
 
 const App = () => {
   return (
-    <div className='h-screen'>
-        <NavBar/>
+    <div className="h-screen">
+      <StreakPointsProvider>
         <Routes>
-            <Route path='/'element={<AuthPage/>} />
-            <Route path='/dashboard' element= {<Dashboard/>} /> 
-
+          <Route path="/" element={<AuthPage />} />
         </Routes>
-    </div>
-  )
-}
 
-export default App
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/Quiz" element={<Quiz />} />
+          <Route path="/Game" element={<Game />} />
+          <Route path="/Project" element={<ProjectCollab />} />
+          <Route path="/EcoScore" element={<EcoScore />} />
+          <Route path="/Map" element={<Map />} />
+          <Route path="/Calculator" element={<Calculator />} />
+          <Route path="/Tracker" element={<Tracker />} />
+          <Route path="/Reminder" element={<AutomatedReminders />} />
+        </Routes>
+      </StreakPointsProvider>
+    </div>
+  );
+};
+
+export default App;
