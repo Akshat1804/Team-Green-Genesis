1c6dcfd4082ef18e72b4b0b26fabe1f7c2057226
Akshat Sharma <86612849+Akshat1804@users.noreply.github.com>
2025-02-09 13:17:00 +0530

    Add files via upload

---
+++ src/components/LoginNav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoginNav= () => {
+  return (
+    <nav id='nav' className='w-full text-white font-bold  h-[10%] flex justify-between items-center p-5    shadow-lg'>
+
+            <h1  className='text-2xl px-[75px]'>CO <sub>2</sub> Scope</h1>
+  </nav>
+  )
+}
+
+export default LoginNav
